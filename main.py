02937b02b745746aa91636b26677b562e5a2fcc2
ArtBulSpb <arturtomsk2005@gmail.com>
2022-09-25 13:32:19 +0300

    Fix BoxLayout

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import Boxlayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 
-#В переменной KV создаём вёрстку
+# В переменной KV создаём вёрстку
 KV = """
-
+MyBL:
+        Label: #вывод
+                font_size:"30sp"
+                text:root.data_label
 """
+
+
+class MyBL(BoxLayout):
+    data_label = StringProperty("Приветствую")
+
+
+class MyApp(App):
+    running = True
+
+    def build(self):
+        return Builder.load_string(KV)
+
+    def on_stop(self):
+        self_running = False
+
+
+MyApp().run()
