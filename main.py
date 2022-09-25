fd3bf399baf2e29894fd8ee508beb7f71a068ffc
ArtBulSpb <arturtomsk2005@gmail.com>
2022-09-25 12:37:24 +0300

    Init commit.

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import Boxlayout
+from kivy.properties import StringProperty
+
+#В переменной KV создаём вёрстку
+KV = """
+
+"""
