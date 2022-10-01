e1f6d12e301e1b68f11985d37c4a04e3151a56e8
ArtBulSpb <arturtomsk2005@gmail.com>
2022-10-01 21:41:21 +0300

    Add on_press behavior for buttons and label

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-#__version__ = “1.0.3”
+
+__version__ = '1.0.3'
+
+import kivy as kivy
+
+requirements = kivy
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -11,35 +16,59 @@ from kivy.uix.textinput import TextInput
 KV = """
 MyBL:
         orientation: "vertical"
-        size_hint: (0.95, 0.95)
+        size_hint: (0.98, 0.98)
         pos_hint: {"center_x": 0.5, "center_y":0.5}
+        padding: 10
+        spacing: 10
+                
         Label: #вывод
                 font_size:"30sp"
                 text:root.data_label
+                
         Button:
                 text: "поиск по названию 1"
                 bold: True
                 background_color:'#9932CC'
                 size_hint: (1,0.5)
-                on_press: root.callback()
+                on_press: root.label_change("поиск по названию 1")
+                line_width: 2
+                elevation_normal: 8  # длинна тени 
+                radius: [20]
+                
         Button:
                 text: "поиск по названию 2"
                 bold: True
                 background_color:'#9932CC'
                 size_hint: (1,0.5)
-                on_press: root.callback()
+                on_press: root.label_change("поиск по названию 2")
+                line_width: 2
         Button:
                 text: "поиск по названию 3"
                 bold: True
                 background_color:'#9932CC'
                 size_hint: (1,0.5)
-                on_press: root.callback()
+                on_press: root.label_change("поиск по названию 3")
+                line_width: 2
+                
+        Button:
+                text: "поиск по названию 4"
+                bold: True
+                background_color:'#9932CC'
+                size_hint: (1,0.5)
+                on_press: root.label_change("поиск по названию 4")
+                radius: [50,]
+                # border: 30,30,30,30
+                
                 
 """
 
 
 class MyBL(BoxLayout):
     data_label = StringProperty("Приветствую!")
+
+    def label_change(self, new_text):
+        self.data_label = new_text
+        print(new_text)
 
     def callback(self):
         print("poisk")
